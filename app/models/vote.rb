6d03363f665e06863cde621b46f82b6f37b9b729
Anna Karingal <anna.karingal@gmail.com>
2015-07-31 11:19:41 -0400

    Validate votes for presence of values & foreign keys; vote values for valid range

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,14 @@
 class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :votable, :polymorphic => true
+
+  validates :votable_id, :votable_type, :user_id, :value, presence: true
+  validate :valid_vote_value
+
+  private
+  def valid_vote_value
+    if value < -1 || value > 1
+      errors.add(value: "cannot be less than -1 or greater than 1")
+    end
+  end
 end

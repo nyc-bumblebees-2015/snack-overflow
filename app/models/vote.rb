e840256ba8f1fcc3fceac24d489dc2cf1d15be22
Anna Karingal <anna.karingal@gmail.com>
2015-07-31 11:39:24 -0400

    Validate vote value with numericality instead of custom validation

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,12 +3,9 @@ class Vote < ActiveRecord::Base
   belongs_to :votable, :polymorphic => true
 
   validates :votable_id, :votable_type, :user_id, :value, presence: true
-  validate :valid_vote_value
+  validate :value, numericality: { only_integer: true,
+                                   greater_than_or_equal_to: -1,
+                                   less_than_or_equal_to: 1
+                                 }
 
-  private
-  def valid_vote_value
-    if value < -1 || value > 1
-      errors.add(value: "cannot be less than -1 or greater than 1")
-    end
-  end
 end

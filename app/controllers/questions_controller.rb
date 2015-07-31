8bb4258cbdd61449b3c8479083c7129432491961
britneywright <blw06g@gmail.com>
2015-07-31 19:58:34 -0400

    uncomment out index and show routes

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,10 @@
 class QuestionsController < ApplicationController
-  # def index
-  #   @recent_questions = Question.recent
-  #   @popular_questions = Question.most_popular
-  # end
+  def index
+    @recent_questions = Question.recent
+    @popular_questions = Question.most_popular
+  end
 
-  # def show
-  #   @question = Question.find_by(id: params[:id])
-  # end
+  def show
+    @question = Question.find_by(id: params[:id])
+  end
 end

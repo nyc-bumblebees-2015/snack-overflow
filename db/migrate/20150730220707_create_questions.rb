450248ccf71534f1b6242ac71b0187855dfb111d
Jason Guzik <jguzik@gmail.com>
2015-07-31 10:29:06 -0400

    fix migrations that I switched up

--- db/migrate/20150730220707_create_questions.rb
+++ db/migrate/20150730220707_create_questions.rb
@@ -1,8 +1,9 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
-      t.integer :question_id
-      t.integer :tag_id
+      t.string :title, null: false
+      t.string :content, null: false
+      t.integer :user_id, null: false
 
       t.timestamps null: false
     end
--- db/migrate/20150730220927_create_questiontags.rb
+++ db/migrate/20150730220927_create_questiontags.rb
@@ -1,9 +1,8 @@
 class CreateQuestiontags < ActiveRecord::Migration
   def change
     create_table :questiontags do |t|
-      t.string :title, null: false
-      t.string :content, null: false
-      t.integer :user_id, null: false
+      t.integer :question_id, null: false
+      t.integer :tag_id, null: false
 
       t.timestamps null: false
     end

e65d5e3b9109133e68503dbc6f672d7f8674f506
Anna Karingal <anna.karingal@gmail.com>
2015-07-31 16:45:45 -0400

    Implement create route for answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,19 @@
 class AnswersController < ApplicationController
+  def create
+    logged_in? 
+    answer = Answer.new(answer_params)
+    question = Question.find_by(id: params[:question_id])
+    answer.question = question
+    if answer.save
+      flash[:success] = "Your answer has been added!"
+    else
+      flash[:error] = answer.errors.full_messages
+    end
+    redirect_to question_path(question)
+  end
+
+  private
+  def answer_params
+    params.require(:answer).permit(:content)
+  end
 end

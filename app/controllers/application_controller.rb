d0c2e43a29b56fbda44cf20b34cfbc93a14d96ac
Jason Guzik <jguzik@gmail.com>
2015-07-31 14:13:21 -0400

    create sessions controller with routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,27 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+
   protect_from_forgery with: :exception
+  helper_method :current_user, :is_current_user?
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    if !session[:user_id]
+      flash[:error] = "You don't have access to this page"
+      redirect_to root_path
+    end
+  end
+
+  def require_authenticated_user(user)
+    unless is_current_user?(user)
+      flash[:error] = "You don't have access to this page"
+      redirect_to root_path
+    end
+  end
+
+  def is_current_user?(user)
+    user && session[:user_id] == user_id
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  root 'questions#index'
+  resources :users
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  get 'logout' => 'sessions#delete'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 

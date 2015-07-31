1676b95f77349979eda519fe07e02d2647f8bb58
britneywright <blw06g@gmail.com>
2015-07-31 11:27:30 -0400

    add comment type factories

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,13 @@
 FactoryGirl.define do
   factory :comment do
     content { Faker::Lorem.paragraph }
+
+    factory :question_comment do
+      commentable_type 'question'
+    end
+
+    factory :answer_comment do
+      commentable_type 'answer'
+    end
   end
 end

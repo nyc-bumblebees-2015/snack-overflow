b749f8553a5dcd42965c6856be3af4c6549ca0a8
britneywright <blw06g@gmail.com>
2015-07-31 11:57:08 -0400

    fix typo

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module Snackoverflow
             routing_specs: false,
             controller_specs: true,
             request_specs: false
-        g.fixture_replacement :factory_girl, dir "spec/factories"
+        g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
     config.active_record.raise_in_transactional_callbacks = true
   end

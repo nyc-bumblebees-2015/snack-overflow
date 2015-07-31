34dbfe012a36c9e2f27d39f2542711499cf7ead3
britneywright <blw06g@gmail.com>
2015-07-31 11:43:17 -0400

    create vote factory

---
+++ spec/factories/votes.rb
@@ -0,0 +1,25 @@
+FactoryGirl.define do
+  factory :vote do
+    association :user
+
+    factory :question_upvote do
+      value 1
+      association :votable, :factory => :question
+    end
+
+    factory :question_downvote do
+      value -1
+      association :votable, :factory => :question
+    end
+
+    factory :answer_upvote do
+      value 1
+      association :votable, :factory => :answer
+    end
+
+    factory :answer_upvote do
+      value -1
+      association :votable, :factory => :answer
+    end
+  end
+end

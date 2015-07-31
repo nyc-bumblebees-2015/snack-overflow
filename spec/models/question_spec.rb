90c675f0b523e676b3e3d471321598021837b5d7
britneywright <blw06g@gmail.com>
2015-07-31 17:38:44 -0400

    add question model tests

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,9 +1,33 @@
 require 'rails_helper'
 
 describe Question do
-  it "belongs to a user" do
+  before(:each) do
     user = create(:user)
-    question = create(:question, user: user)
-    expect(question).to be_valid
+    @question = create(:question, user: user)
   end
+
+  it "is valid with a title, content, and user" do
+    expect(@question).to be_valid
+  end
+
+  it "is invalid without a title" do
+    @question.title = nil
+    expect(@question).to be_invalid
+  end
+
+  it "is invalid without content" do
+    @question.content = nil
+    expect(@question).to be_invalid
+  end
+
+  it "is invalid without a user" do
+    @question.user = nil
+    expect(@question).to be_invalid
+  end
+
+  it "receives an accepted answer among suggested answers"
+  it "cannot have an accepted answer that is not among suggested answers"
+  it "can have many tags"
+  it "can have votes"
+  it "can have many answers"
 end

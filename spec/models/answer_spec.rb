6e1775bef2d15f583e59e4097ed01eb80f4e6e1e
Anna Karingal <anna.karingal@gmail.com>
2015-07-31 19:20:55 -0400

    Add user auth stubs

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -5,7 +5,7 @@ describe Answer do
   let (:test_user) { create(:user) }
   let (:test_question) { create(:question) }
   let (:test_answer) { create(:answer, user: test_user, question: test_question) }
-
+  
   it "belongs to a question and a user and has content" do
     expect(test_answer).to be_valid
   end

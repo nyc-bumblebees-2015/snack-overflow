a9cc906a94393d1cf15963df866c45f81b0487ba
britneywright <blw06g@gmail.com>
2015-07-31 14:31:28 -0400

    change content attributes to text data types

--- db/migrate/20150730220707_create_questions.rb
+++ db/migrate/20150730220707_create_questions.rb
@@ -2,7 +2,7 @@ class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
       t.string :title, null: false
-      t.string :content, null: false
+      t.text :content, null: false
       t.integer :user_id, null: false
 
       t.timestamps null: false

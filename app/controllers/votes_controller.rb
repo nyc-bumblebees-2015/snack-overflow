fbe73a39247cc51ddc203ab403b49b5f3cd12da7
Anna Karingal <anna.karingal@gmail.com>
2015-08-01 15:22:09 -0400

    Implement create route

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,14 @@
 class VotesController < ApplicationController
+  def create
+    if params[:voteable_type] == "Question"
+      voteable = Question.find_by(id: params[:question_id])
+    else
+      voteable = Answer.find_by(id: params[:answer_id])
+    end
+    voteable.votes << Vote.new(value: params[:vote_value], user: current_user)
+    if voteable.save && request.xhr?
+      vote_count = voteable.votes_total
+      render json: {vote_count: vote_count}.to_json
+    end
+  end
 end

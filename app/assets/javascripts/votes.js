f8be3819c855a3b0696ae1347cff82dc61ea1aa9
Anna Karingal <anna.karingal@gmail.com>
2015-08-01 15:28:43 -0400

    Implement downvote for questions

--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -25,6 +25,30 @@ $(document).ready(function(){
 
   });
 
+  $('.question_votes').on('click', 'a.downvote', function(event){
+    event.preventDefault();
+    var questionId = $(this).parent().data().questionId;
+    var $votes_total = $(this).siblings('.votes_total')
+    var url = '/questions/' + questionId.toString() + '/votes';
+    var vote = {
+      voteable_type: 'Question',
+      vote_value: downvoteValue
+    };
+
+    $.ajax({
+      url: url,
+      method: 'post',
+      data: vote,
+      dataType: 'json'
+    }).done(function(response){
+      $votes_total.html(response.vote_count); 
+    }).fail(function(error){
+      console.log("ERROR: ", error);
+    })
+
+  });
+
+
   $('.suggested_answers').on('click', '.answer_votes > a.upvote', function(event){
     event.preventDefault();
     var url = '/answers/' + answerId.toString() + '/votes';

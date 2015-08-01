c201f1214281f86ac1f6f97969eb92ff8a3e9dd2
britneywright <blw06g@gmail.com>
2015-08-01 12:51:03 -0400

    add latest_answer and last_active helper methods

--- app/models/question.rb
+++ app/models/question.rb
@@ -22,6 +22,14 @@ class Question < ActiveRecord::Base
     self.votes.sum("value")
   end
 
+  def latest_answer
+    self.answers.order(created_at: :desc).first
+  end
+
+  def last_active
+    latest_answer.created_at if latest_answer
+  end
+
   private
   def accepted_answer_belongs_to_question
     unless !accepted_answer || accepted_answer.question == self

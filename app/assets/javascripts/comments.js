70725a8fb7fa8c412a4cfad23eb372e8f209d168
Anna Karingal <anna.karingal@gmail.com>
2015-08-01 17:13:16 -0400

    Post comments via ajax

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -5,4 +5,23 @@ $(document).ready(function(){
     $(this).hide();
   });
 
+  $('.comment_form').on('submit', function(event){
+    event.preventDefault();
+    $commentsForm = $(this);
+    $displayComments = $(this).parents().siblings('.display_comments');
+
+    $.ajax({
+      url: event.target.action, 
+      method: event.target.method, 
+      data: $(event.target).serialize(),
+    }).done(function(response){
+      $displayComments.append(response),
+      $commentsForm.hide(),
+      $commentsForm.siblings('.comment_link').show();
+    }).fail(function(error){
+      console.log("ERROR: ", error);
+    });
+
+  });
+
 });
